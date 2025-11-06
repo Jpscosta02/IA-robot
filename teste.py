aeb99d81df1addefa9e1e716d265306765478087
2080421 <2080421@student.uma.pt>
2025-11-06 20:18:34 +0000

    Separar a funcao para escolher quantas vezes se quer atacar.

--- teste.py
+++ teste.py
@@ -11,16 +11,25 @@ mt_ataque = Motor(Port.B)
 
 # Inicialização do EV3
 ev3 = EV3Brick()
-def ataque():
-    ev3.speaker.beep()
-    # Motores
-    mt_ataque.run_until_stalled(800)            # espera 1 segundo
-    wait(500)             # espera 1 segundo
-    mt_ataque.run_angle(1500,-190)    # roda continuamente
-    wait(500)             # espera 1 segundo
-    mt_ataque.run_until_stalled(-2000)  
-    return 
 
-for i in range(4): #4 balls to attack 
-    ataque()
+def ataque() -> None:
+    ev3.speaker.beep()
+    mt_ataque.run_until_stalled(800)      
+    wait(500)             
+    mt_ataque.run_angle(1500,-190)   
+    wait(500)          
+    mt_ataque.run_until_stalled(-2000)
+    ev3.speaker.beep()
+
+def atacar(n:int ) -> None:
+    
+    if(n <= 0 ):
+        return
+
+    for i in range(n):
+        ataque()
+
+    
+
+
 
